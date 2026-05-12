5ba34b1665e847a0c00cc949271197a39ec5d913
EkoBudi14 <ekobudiarto140100@gmail.com>
2026-05-12 23:52:37 +0700

    feat: implement food data models and repository search functionality with dynamic filtering

--- internal/model/food_model.go
+++ internal/model/food_model.go
@@ -10,3 +10,14 @@ type FoodResponse struct {
 	Types       string  `json:"types"`
 	PicturePath string  `json:"picture_path"`
 }
+
+type FoodRequest struct {
+	ID        string  `json:"id" query:"id" validate:"omitempty,max=100"`
+	Limit     int     `json:"limit" query:"limit" validate:"omitempty,min=1"`
+	Name      string  `json:"name" query:"name" validate:"omitempty,max=100"`
+	Types     string  `json:"types" query:"types" validate:"omitempty,max=100"`
+	PriceFrom int     `json:"price_from" query:"price_from" validate:"omitempty,min=0"`
+	PriceTo   int     `json:"price_to" query:"price_to" validate:"omitempty,min=0"`
+	RateFrom  float64 `json:"rate_from" query:"rate_from" validate:"omitempty,min=0"`
+	RateTo    float64 `json:"rate_to" query:"rate_to" validate:"omitempty,min=0"`
+}
--- internal/repository/food_repository.go
+++ internal/repository/food_repository.go
@@ -2,8 +2,10 @@ package repository
 
 import (
 	"golang-clean-architecture/internal/entity"
+	"golang-clean-architecture/internal/model"
 
 	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 type FoodRepository struct {
@@ -14,5 +16,53 @@ type FoodRepository struct {
 func NewFoodRepository(log *logrus.Logger) *FoodRepository {
 	return &FoodRepository{
 		Log: log,
+	}
+}
+
+func (r *FoodRepository) Search(db *gorm.DB, request *model.FoodRequest) ([]entity.Food, error) {
+	var foods []entity.Food
+
+	if err := db.Scopes(r.FilterFood(request)).Limit(request.Limit).Find(&foods).Error; err != nil {
+		return nil, err
+	}
+
+	return foods, nil
+}
+
+func (r *FoodRepository) FilterFood(request *model.FoodRequest) func(tx *gorm.DB) *gorm.DB {
+	return func(tx *gorm.DB) *gorm.DB {
+
+		// Filter by ID (find single food)
+		if request.ID != "" {
+			tx = tx.Where("id = ?", request.ID)
+		}
+
+		// Filter by Name (LIKE)
+		if request.Name != "" {
+			tx = tx.Where("name LIKE ?", "%"+request.Name+"%")
+		}
+
+		// Filter by Types (LIKE)
+		if request.Types != "" {
+			tx = tx.Where("types LIKE ?", "%"+request.Types+"%")
+		}
+
+		// Filter by Price range
+		if request.PriceFrom > 0 {
+			tx = tx.Where("price >= ?", request.PriceFrom)
+		}
+		if request.PriceTo > 0 {
+			tx = tx.Where("price <= ?", request.PriceTo)
+		}
+
+		// Filter by Rate range
+		if request.RateFrom > 0 {
+			tx = tx.Where("rate >= ?", request.RateFrom)
+		}
+		if request.RateTo > 0 {
+			tx = tx.Where("rate <= ?", request.RateTo)
+		}
+
+		return tx
 	}
 }
